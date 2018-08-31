eefe1524eda64ade5747b2d94aecd81d32ae2dc8
leahgwin <leahgwin@gmail.com>
2018-08-31 11:21:04 -0500

    added members to my family dictionary

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace family_dictionary
+{
+    class Program
+    {
+        //Define a Dictionary that contains information about several members of your family. Use the following example as a template.
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
+
+            myFamily.Add("sister", new Dictionary<string, string>(){
+            {"name", "Ansley"},
+            {"age", "21"}
+            });
+            myFamily.Add("mom", new Dictionary<string, string>(){
+            {"name", "Davina"},
+            {"age", "52"}
+            });
+            myFamily.Add("dad", new Dictionary<string, string>(){
+            {"name", "Vern"},
+            {"age", "52"}
+            });
+            myFamily.Add("fiance", new Dictionary<string, string>(){
+            {"name", "Benjamin"},
+            {"age", "26"}
+            });
+            myFamily.Add("dog", new Dictionary<string, string>(){
+            {"name", "Harper"},
+            {"age", "1.5"}
+            });
+        }
+    }
+}
